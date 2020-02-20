2b5601273571aa8f9119ac70dd48d01e9da015d9
Marin Balabanov <marincomics@gmail.com>
2020-02-20 10:12:31 +0100

    First jasmine example

---
+++ 01_jasmine/spec/HelloWorldSpec.js
@@ -0,0 +1,7 @@
+describe("Hello World", function(){
+       // what it should do
+       it("should Return Hello world",function(){
+               //expect something
+               expect(helloworld()).toEqual('Hello World');
+       });
+});
